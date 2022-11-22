9ff2301e716631c1177fa2e6c5329d8eff54ec54
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-11-22 12:27:33 +0900

    feat) BindingResult 클래스를 활용한 입력검증결과 출력

--- src/main/java/com/echo/controller/MusicController.java
+++ src/main/java/com/echo/controller/MusicController.java
@@ -2,21 +2,37 @@ package com.echo.controller;
 
 import com.echo.controller.dto.MusicRegisterIn;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class MusicController {
 
     @PostMapping("/musics")
-    public String post(@RequestBody @Valid MusicRegisterIn registerIn) throws Exception {
+    public Map<String, String> post(@RequestBody @Valid MusicRegisterIn registerIn, BindingResult result) {
         log.info("registerIn={}", registerIn.toString());
-        return "Hello World";
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String invalidFieldName = firstFieldError.getField(); // title
+            String errorMessage = firstFieldError.getDefaultMessage();// 에러 메시지
+
+            Map<String, String> error = new HashMap<>();
+            error.put(invalidFieldName, errorMessage);
+            return error;
+        }
+
+        return Map.of();
     }
 
     @GetMapping("/musics")
